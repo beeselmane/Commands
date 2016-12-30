dc0ad715be81c820bf8609819587629a12d8dc04
beeselmane <beeselmane@gmail.com>
2016-12-29 22:17:18 -0500

    revert

--- car_1/car/main.c
+++
@@ -1,8 +0,0 @@
-#include <stdio.h>
-#include "car.h"
-
-int main(int argc, const char *const *argv)
-{
-    printf("%d\n", CArchiveCreateWithVersion(kCArchiveVersion_4_B, argv[1], argv[2]));
-    return 0;
-}
--- iostool/iostool/main.c
+++
@@ -1,8 +0,0 @@
-extern void do_lwvm(void);
-
-int main(int argc, const char *const *argv)
-{
-    do_lwvm();
-
-    return 0;
-}
